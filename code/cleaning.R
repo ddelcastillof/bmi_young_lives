9f7a26d72614ba6207f0a2ec70a69a1129dc4bf7
ddelcastf <ddelcast@uw.edu>
2025-06-21 00:51:03 -0700

    Placeholders for code

---
+++ code/cleaning.R
@@ -0,0 +1,17 @@
+# ---
+# Cleaning script for the datasets
+# Author: Darwin Del Castillo
+# Date: `r Sys.Date()`
+# ---
+
+# Load necessary libraries
+pacman::p_load(
+  dplyr,
+  tidyr,
+  stringr,
+  lubridate,
+  readr,
+  skimr,
+  janitor
+)
+
---
+++ code/master.R
@@ -0,0 +1,11 @@
+# ---
+# Master script for data cleaning and processing
+# Author: Darwin Del Castillo
+# Date: `r Sys.Date()`
+# ---
+
+# Sourcing scripts (placeholders)
+source("code/01_load_data.R")
+source("code/02_clean_data.R")
+source("code/03_clean_data_2.R")
+source("code/04_clean_data_3.R")
